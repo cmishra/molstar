b15e979c747fdb540d89330cfdaca98c6e62f92b
Chetan Mishra <chetan@rhizomeai.com>
2025-04-12 20:53:37 -0400

    added build artifacts for easy custom packageb uilding

---
+++ lib/examples/mvs-kinase-story/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2025 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ *
+ * @author David Sehnal <david.sehnal@gmail.com>
+ */
+import './index.html';
+import './elements/snapshot-markdown';
+import './elements/viewer';
+import '../../mol-plugin-ui/skin/light.scss';
+import './styles.scss';
+export declare class MolComponents {
+    getContext(name?: string): any;
+}
